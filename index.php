794a77f32dee0bcb9deac988dc54d615b422d879
Flávio Martins <fhcmartins@gmail.com>
2019-03-05 11:39:59 -0300

    Criação do menu e do banner (somente imagem).

--- index.php
+++ index.php
@@ -1,14 +1,33 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>MediCenter</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=0">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css">
     <script src="main.js"></script>
+
+    <title>MediCenter</title>
 </head>
 <body>
-    <h1>Página de teste</h1>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <a href=""><img src="assets/images/logo_clinica.png" alt="Logo MediCenter" width="200px"></a>
+            </div>
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li class="active"><a href="">HOME</a></li> <!-- O class active irá mostar qual o menu está ativo -->
+                        <li><a href="">HOME</a></li>
+                        <li><a href="">HOME</a></li>
+                        <li><a href="">HOME</a></li>
+                        <li><a href="">HOME</a></li>
+                    </ul>
+                
+                </nav>
+            </div>
+        </div>
+    </header> 
 </body>
 </html>
